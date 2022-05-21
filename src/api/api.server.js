f43657f88007fb36abc9668ba61f9fe74e754ec1
Gabriel <gmamanican@unsa.edu.pe>
2022-05-21 10:25:07 -0500

    Changing backend

--- src/api/api.server.js
+++ src/api/api.server.js
@@ -1,3 +1,4 @@
-const API_SERVER = "http://localhost:5000"; // LOCAL
+// const API_SERVER = "http://localhost:5000"; // LOCAL
+const API_SERVER = "https://hotel-api-proyect.herokuapp.com"; // HEROKU
 
 export default API_SERVER;
